fbc1d0083368567a5fb2a9f4cc2c1f1aed53bf40
Raphael Staffner <raphael.staffner@hak-kitz.ac.at>
2022-11-25 09:28:48 +0100

    Pokegotchi Datei hinzugefügtrgthtr

---
+++ Pokegotchi_SWP/Pokegotchi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokegotchi_SWP
+{
+    internal class Pokegotchi
+    {
+        private string name;
+
+    }
+}
