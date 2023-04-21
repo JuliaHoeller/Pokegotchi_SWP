c7da13f862465fc9546d3d7d5bee7a4721ac963c
Raphael Staffner <raphael.staffner@hak-kitz.ac.at>
2023-04-21 09:47:22 +0200

    Damage angefangen

--- Pokegotchi_SWP/DamageCalculation.cs
+++ Pokegotchi_SWP/DamageCalculation.cs
@@ -8,9 +8,38 @@ namespace Pokegotchi_SWP
 {
     internal class DamageCalculation
     {
-        public int Calculate(int Attackstat, int EnemyDefense, int AttackDamage, int CritDamage, int Critchance, string AttackType, string DefenseTyp1, string DefenseTyp2)
+        public int Calculate(int Attackstat, int EnemyDefense, int AttackDamage, int Critchance, string AttackType, string UserType, string DefenseTyp1)
         {
             int Damage = 0;
+            double CritMultipliyer = 1;
+            double STAB = 1;
+            double TypeMultipliyer;
+
+            int Base = AttackDamage;
+
+            Random rnd = new Random();
+            int CritOrNot = rnd.Next(1, 100);
+            if(CritOrNot <= Critchance)
+            {
+                CritMultipliyer = 1.35;
+            }
+            else
+            {
+                CritMultipliyer = 1;
+            }
+
+            if (AttackType.Equals(UserType))
+            {
+                STAB = 1.5;
+            }
+            else
+            {
+                STAB = 1;
+            }
+
+            Damage = Convert.ToInt32(Base * Attackstat / EnemyDefense * CritMultipliyer);
+            
+            
 
 
 
