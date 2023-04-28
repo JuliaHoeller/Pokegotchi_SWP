6d365e7b8a40fa383d559a1cf90135d98c8eaefc
Jaqueline Pfister <jaqueline.pfister@hak-kitz.ac.at>
2023-04-28 09:35:47 +0200

    Grobe Fertigstellung PokegotchiSelection Form

--- Pokegotchi_SWP/f_PokegotchiSelection.Designer.cs
+++ Pokegotchi_SWP/f_PokegotchiSelection.Designer.cs
@@ -34,6 +34,14 @@
             pbPokegotchi2 = new PictureBox();
             lblPokegotchi1Name = new Label();
             lblPokegotchi1Level = new Label();
+            lblPokegotchi2Name = new Label();
+            lblPokegotchi3Name = new Label();
+            lblPokegotchi4Name = new Label();
+            lblPokegotchi2Level = new Label();
+            lblPokegotchi3Level = new Label();
+            lblPokegotchi4Level = new Label();
+            btnStartFight = new Button();
+            cblSelectPokegotchi = new CheckedListBox();
             ((System.ComponentModel.ISupportInitialize)pbPokegotchi1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbPokegotchi4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbPokegotchi3).BeginInit();
@@ -83,26 +91,116 @@
             // lblPokegotchi1Name
             // 
             lblPokegotchi1Name.AutoSize = true;
+            lblPokegotchi1Name.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
             lblPokegotchi1Name.Location = new Point(427, 114);
             lblPokegotchi1Name.Name = "lblPokegotchi1Name";
-            lblPokegotchi1Name.Size = new Size(33, 15);
+            lblPokegotchi1Name.Size = new Size(33, 16);
             lblPokegotchi1Name.TabIndex = 4;
             lblPokegotchi1Name.Text = "Venti";
             // 
             // lblPokegotchi1Level
             // 
             lblPokegotchi1Level.AutoSize = true;
+            lblPokegotchi1Level.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
             lblPokegotchi1Level.Location = new Point(427, 129);
             lblPokegotchi1Level.Name = "lblPokegotchi1Level";
-            lblPokegotchi1Level.Size = new Size(51, 15);
+            lblPokegotchi1Level.Size = new Size(55, 16);
             lblPokegotchi1Level.TabIndex = 5;
             lblPokegotchi1Level.Text = "Lvl: 3000";
+            // 
+            // lblPokegotchi2Name
+            // 
+            lblPokegotchi2Name.AutoSize = true;
+            lblPokegotchi2Name.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi2Name.Location = new Point(641, 114);
+            lblPokegotchi2Name.Name = "lblPokegotchi2Name";
+            lblPokegotchi2Name.Size = new Size(33, 16);
+            lblPokegotchi2Name.TabIndex = 6;
+            lblPokegotchi2Name.Text = "Venti";
+            // 
+            // lblPokegotchi3Name
+            // 
+            lblPokegotchi3Name.AutoSize = true;
+            lblPokegotchi3Name.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi3Name.Location = new Point(427, 266);
+            lblPokegotchi3Name.Name = "lblPokegotchi3Name";
+            lblPokegotchi3Name.Size = new Size(33, 16);
+            lblPokegotchi3Name.TabIndex = 7;
+            lblPokegotchi3Name.Text = "Venti";
+            // 
+            // lblPokegotchi4Name
+            // 
+            lblPokegotchi4Name.AutoSize = true;
+            lblPokegotchi4Name.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi4Name.Location = new Point(641, 266);
+            lblPokegotchi4Name.Name = "lblPokegotchi4Name";
+            lblPokegotchi4Name.Size = new Size(33, 16);
+            lblPokegotchi4Name.TabIndex = 8;
+            lblPokegotchi4Name.Text = "Venti";
+            // 
+            // lblPokegotchi2Level
+            // 
+            lblPokegotchi2Level.AutoSize = true;
+            lblPokegotchi2Level.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi2Level.Location = new Point(641, 129);
+            lblPokegotchi2Level.Name = "lblPokegotchi2Level";
+            lblPokegotchi2Level.Size = new Size(55, 16);
+            lblPokegotchi2Level.TabIndex = 9;
+            lblPokegotchi2Level.Text = "Lvl: 3000";
+            // 
+            // lblPokegotchi3Level
+            // 
+            lblPokegotchi3Level.AutoSize = true;
+            lblPokegotchi3Level.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi3Level.Location = new Point(427, 281);
+            lblPokegotchi3Level.Name = "lblPokegotchi3Level";
+            lblPokegotchi3Level.Size = new Size(55, 16);
+            lblPokegotchi3Level.TabIndex = 10;
+            lblPokegotchi3Level.Text = "Lvl: 3000";
+            // 
+            // lblPokegotchi4Level
+            // 
+            lblPokegotchi4Level.AutoSize = true;
+            lblPokegotchi4Level.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPokegotchi4Level.Location = new Point(641, 281);
+            lblPokegotchi4Level.Name = "lblPokegotchi4Level";
+            lblPokegotchi4Level.Size = new Size(55, 16);
+            lblPokegotchi4Level.TabIndex = 11;
+            lblPokegotchi4Level.Text = "Lvl: 3000";
+            // 
+            // btnStartFight
+            // 
+            btnStartFight.Font = new Font("Franklin Gothic Book", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnStartFight.Location = new Point(83, 302);
+            btnStartFight.Name = "btnStartFight";
+            btnStartFight.Size = new Size(178, 23);
+            btnStartFight.TabIndex = 12;
+            btnStartFight.Text = "Kampf starten";
+            btnStartFight.UseVisualStyleBackColor = true;
+            // 
+            // cblSelectPokegotchi
+            // 
+            cblSelectPokegotchi.Font = new Font("Franklin Gothic Book", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            cblSelectPokegotchi.FormattingEnabled = true;
+            cblSelectPokegotchi.Items.AddRange(new object[] { "Venti (Lvl. 3000.)", "Venti (Lvl. 3000.)" });
+            cblSelectPokegotchi.Location = new Point(41, 22);
+            cblSelectPokegotchi.Name = "cblSelectPokegotchi";
+            cblSelectPokegotchi.Size = new Size(271, 256);
+            cblSelectPokegotchi.TabIndex = 13;
             // 
             // f_PokegotchiSelection
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 337);
+            Controls.Add(cblSelectPokegotchi);
+            Controls.Add(btnStartFight);
+            Controls.Add(lblPokegotchi4Level);
+            Controls.Add(lblPokegotchi3Level);
+            Controls.Add(lblPokegotchi2Level);
+            Controls.Add(lblPokegotchi4Name);
+            Controls.Add(lblPokegotchi3Name);
+            Controls.Add(lblPokegotchi2Name);
             Controls.Add(lblPokegotchi1Level);
             Controls.Add(lblPokegotchi1Name);
             Controls.Add(pbPokegotchi2);
@@ -127,5 +225,13 @@
         private PictureBox pbPokegotchi2;
         private Label lblPokegotchi1Name;
         private Label lblPokegotchi1Level;
+        private Label lblPokegotchi2Name;
+        private Label lblPokegotchi3Name;
+        private Label lblPokegotchi4Name;
+        private Label lblPokegotchi2Level;
+        private Label lblPokegotchi3Level;
+        private Label lblPokegotchi4Level;
+        private Button btnStartFight;
+        private CheckedListBox cblSelectPokegotchi;
     }
 }
